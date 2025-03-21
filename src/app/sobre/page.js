c7b0d50d2b4a6343d684bab1195bc7e33ef5c5e6
Joaquim Pessôa Filho <joaquimp@users.noreply.github.com>
2025-03-21 23:53:27 +0000

    primeiro commit

---
+++ src/app/sobre/page.js
@@ -0,0 +1,12 @@
+export default function Sobre() {
+    return (
+        <main>
+            <Nome />
+            <Nome />
+        </main>
+            )
+}
+
+function Nome() {
+    return <h1>Joaquim</h1>
+}
